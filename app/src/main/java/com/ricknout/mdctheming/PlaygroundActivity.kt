fd7504157db016ff3cd0792ed43e516377f20f2e
Nick Rout <nicholasrout@gmail.com>
2019-01-25 16:42:32 +0200

    Rename main Activity to playground

--- app/src/main/java/com/ricknout/mdctheming/PlaygroundActivity.kt
+++ app/src/main/java/com/ricknout/mdctheming/PlaygroundActivity.kt
@@ -3,10 +3,10 @@ package com.ricknout.mdctheming
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PlaygroundActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_playground)
     }
 }
