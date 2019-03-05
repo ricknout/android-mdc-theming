126e5d8cd3e397da7f20f962f52d8d62f80a0728
Nick Rout <nicholasrout@gmail.com>
2019-03-05 19:39:33 +0200

    Add comment linking to BottomAppBar cradleVerticalOffset bug issue

--- app/src/main/java/com/ricknout/mdctheming/bottomappbar/BottomAppBarActivity.kt
+++ app/src/main/java/com/ricknout/mdctheming/bottomappbar/BottomAppBarActivity.kt
@@ -54,14 +54,16 @@ class BottomAppBarActivity : AppCompatActivity() {
         increaseFabCradleVerticalOffsetButton.setOnClickListener {
             val cradleVerticalOffset = (bottomAppBar.cradleVerticalOffset + oneDp).coerceIn(0f, 16 * oneDp)
             bottomAppBar.cradleVerticalOffset = cradleVerticalOffset
-            // The above does not update the position of the FAB :-( It appears to require a layout,
+            // BUG: The above does not update the position of the FAB :-( It appears to require a layout,
             // but even the setter does not change the internal fabVerticalOffset property...
+            // See: https://issuetracker.google.com/issues/127290685
         }
         decreaseFabCradleVerticalOffsetButton.setOnClickListener {
             val cradleVerticalOffset = (bottomAppBar.cradleVerticalOffset - oneDp).coerceIn(0f, 16 * oneDp)
             bottomAppBar.cradleVerticalOffset = cradleVerticalOffset
-            // The above does not update the position of the FAB :-( It appears to require a layout,
+            // BUG: The above does not update the position of the FAB :-( It appears to require a layout,
             // but even the setter does not change the internal fabVerticalOffset property...
+            // See: https://issuetracker.google.com/issues/127290685
         }
     }
 
